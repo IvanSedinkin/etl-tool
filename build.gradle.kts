2c23aeacbd8c90abab9b784c6efe67ca6333929d
sedinkin.i <sedinkin.i@spectrumdata.ru>
2023-08-21 15:02:04 +0500

    configurate gradle to kotlin project

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,24 +4,11 @@ plugins {
 }
 
 group = "etl.org"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
 kotlin {
     jvmToolchain(11)
-}
-
-application {
-    mainClass.set("MainKt")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,6 @@
 rootProject.name = "etl-project"
 include("parser")
 include("parser")
+include("parser")
+include("parser")
+include("parser")
